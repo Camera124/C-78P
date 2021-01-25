2f723dd1ee35d48fb75471cfb62d71acc1060542
Camera124 <75793966+Camera124@users.noreply.github.com>
2021-01-25 20:07:07 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function next(){
+    var name_member=["Koushik Rout","Debasmita Rout","Aryan Rout","Alethia Rout"];
+    var img_member=["father.jpg","mother.jpg","son.jpg","sister.jpg"];
+document.getElementById("book").innerHTML=name_member.nextslide();
+}
